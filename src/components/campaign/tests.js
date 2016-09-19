1df24a7b29fde777c71f5cd5dfe76e7f19d40f07
Arthur Yip <arthuryip723@gmail.com>
2016-09-19 18:53:45 -0400

    creating tests

--- src/components/campaign/tests.js
+++ src/components/campaign/tests.js
@@ -20,7 +20,16 @@ describe('Campaign Component', function() {
         );
         const r = new routes();
         r.list();
-        expect($('#campaignList .one_block .bottom_pr').html()).to.equal('<p>Price per share: $2.00</p><p>Number of shares: 125,000</p>');
+        expect($('#campaignList .one_block .bottom_pr:eq(6)').html()).to.equal('<p>Price per share: $2.00</p><p>Number of shares: 125,000</p>');
+        expect($('#campaignList .one_block .place').html()).to.equal('<i class="fa fa-map-marker"></i>New York NY');
+        expect($('#campaignList .one_block .description').html()).to.equal('<p>about us for BSX</p>');
+        expect($('#campaignList .one_block .progress_bar_container .left_text').html()).to.equal('0 % Funded');
+        expect($('#campaignList .one_block .progress_bar_container .right_text').html()).to.equal('<b>$0 </b>of $250,000');
+        expect($('#campaignList .one_block .more_inform .small_text').html()).to.equal('<p>tagline for BSX</p>');
+        expect($('#campaignList .one_block .more_inform .title').html()).to.equal('<h3>Valuation: $2,000,000</h3>');
+        expect($('#campaignList .one_block > .title h3').html()).to.equal('BSX LVL Hydration Band / this is a test for company name');
+        expect($('#campaignList .one_block .bottom_text .left_desc').html()).to.equal('<b>Security:</b>Common Equity');
+        expect($('#campaignList .one_block .bottom_text .right_desc').html()).to.equal('<b>Industry: </b>Lorem ipsum');
         expect(ajaxStub.called).eq.true;
         ajaxStub.restore();
     }),
